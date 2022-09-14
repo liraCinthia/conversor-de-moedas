703ed87cb4f90cb4586983c0c31876a546148c2c
Mandic <mandicneves@gmail.com>
2022-09-14 20:02:23 -0300

    adicionando js,css e html

---
+++ conversor.js
@@ -0,0 +1,12 @@
+function Converter() {
+  var valorElemento = document.getElementById('valor');
+  var valor = valorElemento.value;
+  var valorEmEuroNumerico = parseFloat(valor);
+
+  var valorEmReal = valorEmEuroNumerico * 5;
+  //console.log(valorEmReal);
+
+  var elementoValorConvertido = document.getElementById('valorConvertido');
+  var valorConvertido = 'O resultado em euro é R$ ' + valorEmReal;
+  elementoValorConvertido.innerHTML = valorConvertido;
+}
